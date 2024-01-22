b91b345acd1399c5a9d3a584946e5b2ebd1fc8b8
SanyFaysal <tfoysalahmed@gmail.com>
2024-01-23 02:10:20 +0600

    [DONE]- top banner

--- src/components/home_component/TopCategories.tsx
+++ src/components/home_component/TopCategories.tsx
@@ -11,7 +11,6 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from "@/components/ui/carousel"
-import Image from "next/image";
 export default function TopCategories() {
     const { data }: any = useGetAllCategoriesQuery(undefined);
     return (
@@ -26,20 +25,10 @@ export default function TopCategories() {
             >
                 <CarouselContent>
 
-                    {data?.data?.map((category: ICategory) => <CarouselItem key={category?.id} className="md:basis-1/2 lg:basis-1/6 rounded-lg ">
-                        {/* <CategoryCard category={category} key={category?.id} /> */}
-                        <div className="bg-gray-50 px-3 py-2 rounded-lg text-center ">
-                            <div className="flex justify-center w-min px-2">
-                                <Image src={category?.image} width='0' height='0'
-                                    style={{ width: '80%', height: '90px' }}
-                                    className="rounded  font-semibold"
-                                    alt="Category Image" />
-                            </div>
-                            <div className="w-[20px] flex justify-center h-11 ">
-                                <p className="text-center w-full h-full">{category?.title?.split(' ')?.[0]} <br /> {category?.title?.split(' ')?.[1]} {category?.title?.split(' ')?.[2]}</p>
-                            </div>
-                        </div>
-                    </CarouselItem>)}
+                    {data?.data?.map((category: ICategory) =>
+                    (<CarouselItem key={category?.id} className="md:basis-1/2 lg:basis-1/6 rounded-lg ">
+                        <CategoryCard category={category} />
+                    </CarouselItem>))}
                 </CarouselContent>
                 <CarouselPrevious className=" border-0 text-sky-500" />
                 <CarouselNext className=" border-0 text-sky-500" />
--- src/components/ui/CategoryCard.tsx
+++ src/components/ui/CategoryCard.tsx
@@ -5,16 +5,17 @@ import { ICategory } from "@/types/data";
 import Image from "next/image";
 
 export default function CategoryCard({ category }: { category: ICategory }) {
-
+    console.log({ category })
     return (
         <div className="bg-gray-50 px-3 py-2 rounded-lg text-center ">
-            <div className="flex justify-center w-min px-2">
-                <Image src={category?.image} width='0' height='0'
+            <div className="flex justify-center  px-2">
+                {/* <Image src={'http://res.cloudinary.com/dfcztmnvh/image/upload/v1705694272/orbkrp9z0ufm7jxspczy.jpg'} width={100} height={100}
                     style={{ width: '80%', height: '90px' }}
-                    className="rounded  font-semibold"
-                    alt="Category Image" />
+                     className="rounded  font-semibold"
+                    alt="Category Image" /> */}
+                <img src={category.image} className="w-[100px] h-[80px]" alt="" />
             </div>
-            <div className="w-[20px] flex justify-center h-11 ">
+            <div className=" flex justify-center text-center h-11 ">
                 <p className="text-center w-full h-full">{category?.title?.split(' ')?.[0]} <br /> {category?.title?.split(' ')?.[1]} {category?.title?.split(' ')?.[2]}</p>
             </div>
         </div>
