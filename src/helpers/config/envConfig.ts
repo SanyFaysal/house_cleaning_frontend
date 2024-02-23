f7a2f6b39c2bcbb604ca71c2497f461ca8d11d4e
SanyFaysal <tfoysalahmed@gmail.com>
2024-02-23 18:04:13 +0600

    [UPDATE] backedn url

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,4 +1,4 @@
 export const getBaseUrl = (): string => {
 
-    return process.env.NEXT_PUBLIC_API_BASE_URL || "https://home-sync-solution.onrender.com/api/v1"
+    return process.env.NEXT_PUBLIC_API_BASE_URL || "https://house-cleaning-backend-rcxdiixez-sanyfaysal.vercel.app/api/v1"
 }
