af75bfc3a16f0ca436e774970921fdd034ea8281
Abu Sani Faysal <97028772+SanyFaysal@users.noreply.github.com>
2024-02-25 15:59:05 +0600

    Update envConfig.ts

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,4 +1,4 @@
 export const getBaseUrl = (): string => {
 
-    return process.env.NEXT_PUBLIC_API_BASE_URL || "https://house-cleaning-backend-rcxdiixez-sanyfaysal.vercel.app/api/v1"
+    return "https://house-cleaning-backend-rcxdiixez-sanyfaysal.vercel.app/api/v1"
 }
