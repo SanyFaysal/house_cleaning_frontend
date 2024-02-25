f138909d43ef3d9f9e06bb02202e140252bfd033
SanyFaysal <tfoysalahmed@gmail.com>
2024-02-25 19:00:50 +0600

    [UPDATE] update config

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,9 +1,3 @@
 export const getBaseUrl = (): string => {
-<<<<<<< HEAD
     return "https://house-cleaning-backend.vercel.app/api/v1"
 }
-=======
-
-    return "https://house-cleaning-backend.vercel.app/api/v1"
-}
->>>>>>> 27b52f9393a23eb9a28caf525ae0092dcd5b5da9
